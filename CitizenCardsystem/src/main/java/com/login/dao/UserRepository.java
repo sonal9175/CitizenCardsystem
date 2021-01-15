dc04476a65020150984330fa27b5f7a88a8d5f17
sonal9175 <77433642+sonal9175@users.noreply.github.com>
2021-01-15 09:45:07 +0530

    Delete UserRepository.java

--- CitizenCardsystem/src/main/java/com/login/dao/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.login.dao;
-
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.login.model.User;
-public interface UserRepository  extends CrudRepository<User, String>{
-	
-}
